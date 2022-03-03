4468ec76508a1fd149e09811c627e02e2f7f6cb6
gramsa23 <89297908+gramsa23@users.noreply.github.com>
2022-03-03 23:40:57 +0300

    last

--- BusinessLayer/Concrete/CategoryManager.cs
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -1,33 +1,41 @@
 using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
 
 namespace BusinessLayer.Concrete
 {
     public class CategoryManager : ICategoryService
     {
+        ICategoryDal _categoryDal;
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
         public void AddCategory(Category category)
         {
-            
+            _categoryDal.Insert(category);
         }
 
         public void DeleteCategory(Category category)
         {
-            
+            _categoryDal.Delete(category);
         }
 
         public Category GetCategoryById(int id)
         {
-            throw new NotImplementedException();
+            return _categoryDal.GetById(id);
         }
 
         public IEnumerable<Category> GetList()
         {
-            throw new NotImplementedException();
+            return _categoryDal.GetListAll();
         }
 
         public void UpdateCategory(Category category)
         {
-            
+            _categoryDal.Update(category);
         }
     }
 }
